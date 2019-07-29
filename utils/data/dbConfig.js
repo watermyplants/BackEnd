cc775db6d1135a18c6500d51834ad034f7aa12b3
trucane <trufashionista2013@gmail.com>
2019-07-29 11:13:33 -0500

    WIP heroku issues

--- utils/data/dbConfig.js
+++ utils/data/dbConfig.js
@@ -1,4 +1,6 @@
 const knex = require('knex');
 const connection = require('../../knexfile');
 
-module.exports = knex(connection.development);
+const dbEnv= process.env.DB_ENV || 'development'
+
+module.exports = knex(connection[dbEnv]);
