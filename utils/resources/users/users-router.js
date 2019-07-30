db4f3c4f38249972a0b0cc206d6e8d80ef1a7fda
trucane <trufashionista2013@gmail.com>
2019-07-30 08:21:07 -0500

    WIP posting to Heroku issue 6 getting error log

--- utils/resources/users/users-router.js
+++ utils/resources/users/users-router.js
@@ -40,7 +40,7 @@ router.post('/register', async (req,res) =>{
             res.status(200).json(user)
             
         }catch(error){
-            res.status(500).json({error:"could not register"})
+            res.status(500).json(error, 'could not register')
         }
     }
 });
