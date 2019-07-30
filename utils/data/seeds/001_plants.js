a48049c2a7b9c45397665154842eb037d2edc392
trucane <trufashionista2013@gmail.com>
2019-07-30 10:55:58 -0500

    WIP Heroku issue 9 refactored seeds file

--- utils/data/seeds/001_plants.js
+++ utils/data/seeds/001_plants.js
@@ -1,10 +1,6 @@
 exports.seed = function(knex) {
-    return knex('plants')
-    .then(function () {
-      // Inserts seed entries
       return knex('plants').insert([
         {name: 'weed', user_id:1 },
 
       ]);
-    });
 };
